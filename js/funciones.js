938a2d55ea36bba511327421c0537c999df811d2
SIgnacioRoccuzzo <129063696+SIgnacioRoccuzzo@users.noreply.github.com>
2023-06-05 00:22:17 +0200

    create funciones de borrar interfaz y array

--- js/funciones.js
+++ js/funciones.js
@@ -16,7 +16,22 @@ function saveTareas(pList, pTareas) {
     }
     return 'tarea duplicada'
 }
+function deleteItemArray(pId, pList) {
 
+    let posicionBorrar = pList.findIndex(tarea => tarea.id === pId);
+    if (posicionBorrar !== -1) {
+        pList.splice(posicionBorrar, 1);
+    }
+    console.log(pList);
+}
+
+function deleteItem(event) {
+    event.preventDefault()
+    let id = parseInt(event.target.dataset.id)
+    const articleDelete = event.target.parentNode.parentNode;
+    articleDelete.parentNode.removeChild(articleDelete);
+    deleteItemArray(id, listaTareas);
+}
 
 
 //fx pintarTareas
@@ -26,29 +41,24 @@ function printOneTarea(pTarea, pDom) {
     const ul = document.createElement('ul');
     ul.className = "list-group"
     const li = document.createElement('li');
-    li.className = "list-group-item d-flex justify-content-between"
+    li.className = "list-group-item"
     li.textContent = pTarea.titulo;
     const button = document.createElement('button');
+    button.addEventListener('click', deleteItem)
     button.className = "btn btn-outline-danger";
     button.textContent = 'Eliminar';
-
-
-
 
     article.append(ul);
     ul.append(li, button)
     pDom.appendChild(article);
 
-
-
 }
-
 
 function getDataTask(event) {
     event.preventDefault();
 
     if (inputTask.value === "" || selectTask.value === "") {
-        alert('quilombo')
+        alert('debes introducir una tarea')
     }
     const newTarea = {
         id: id,
@@ -63,24 +73,61 @@ function getDataTask(event) {
         printOneTarea(newTarea, sectionTareas)
         id++;
 
-        //   event.target.reset();
+        event.target.reset();
     } else {
         alert(guardado);
-        //event.target.style.border = '3px solid tomato';
+
     }
 
 
 }
 //primera fx
 
-function printTareas(pLista, pDom) {
 
+function printTareas(pLista, pDom) {
     pLista.forEach(tarea => printOneTarea(tarea, pDom));
 
 }
 printTareas(listaTareas, sectionTareas);
 
+//filtrar por prioridad
 
 
+/**function filterByPrioridad(pListTareas, pPrioridades) {
+    return pListTareas.filter(tarea => tarea.prioridad.toLowerChase() === pPrioridades.toLowerChase());
+}*/
+function filterByPrioridad(pListTareas, pPrioridades) {
+    const filterList = [];
+
+    for (let tarea of pListTareas) {
+        if (tarea.prioridad.toLowerCase() === pPrioridades.toLowerCase()) {
+            filterList[filterList.length] = tarea;
+        }
+    }
+
+    return filterList;
+}
+
+const selectFilter = document.querySelector('#filter-tareas');
+selectFilter.addEventListener('change', getPrioridades);
+
+
+function getPrioridades(event) {
+    let listaFiltrada = filterByPrioridad
+        (listaTareas, event.target.value);
+    printTareas(listaFiltrada, sectionTareas)
+}
+
+
+
+const inputFilter = document.querySelector('#find-tarea');
+inputFilter.addEventListener('input', getTitulo);
+
+function getTitulo(event) {
+    let palabraBuscar = event.target.value;
+    let listaFiltrada = filterByWord(listaTareas, palabraBuscar);
+    printTareas(listaFiltrada, sectionTareas);
+
+}
 
 
