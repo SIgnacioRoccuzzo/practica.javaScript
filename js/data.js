062737645197ab7a122a1199cf8d169ddb26f84f
SIgnacioRoccuzzo <129063696+SIgnacioRoccuzzo@users.noreply.github.com>
2023-06-04 13:49:13 +0200

    creacion de un array

---
+++ js/data.js
@@ -0,0 +1,17 @@
+let listaTareas = [
+    {
+        idTarea: 1,
+        titulo: 'Estudiar Javascript',
+        prioridad: 'urgente',
+    },
+    {
+        idTarea: 2,
+        titulo: 'Dormir',
+        prioridad: 'diaria',
+    },
+    {
+        idTarea: 3,
+        titulo: 'Salir a comer',
+        prioridad: 'mensual'
+    }
+]
